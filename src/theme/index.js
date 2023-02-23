869c1dd7f815aba1930451105bfce766ed32269f
nlacerdagabriel <devlacerdagabriel@hotmail.com>
2023-02-23 11:42:23 -0300

    structured folders

---
+++ src/theme/index.js
@@ -0,0 +1,45 @@
+export const theme = {
+  colors: {
+    primary: "",
+    secondary: "",
+    text: "",
+    white: "",
+    primary_gradient: "",
+    secondary_gradient: "",
+  },
+  size: {
+    fonts: {
+      heading: {
+        xl: {
+          size: "3.5rem",
+          line_height: "4.75rem",
+        },
+        l: {
+          size: "2.5rem",
+          line_height: "3.1rem",
+        },
+        m: {
+          size: "2rem",
+          line_height: "2.5rem",
+        },
+        s: {
+          size: "1.5rem",
+          line_height: "1.75rem",
+        },
+      },
+      body: {
+        m: {
+          size: "1.1rem",
+          line_height: "1.75rem",
+        },
+        s: {
+          size: "1rem",
+          line_height: "75rem",
+        },
+      },
+    },
+    container: {
+      xl: "1110px",
+    },
+  },
+};
